238c739a1ebfb5a176eb318bb7bc7ed888b66c3c
Jimmy Liu <sicong.liu98@gmail.com>
2015-09-05 13:36:25 -0400

    capital one wrapper

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var wit = require('node-wit');
 var ACCESS_TOKEN = "IBENYNTQT2C36HSUMM6Q5EGBPSJ7EUUU";
+var CAPONE_KEY = "5299a04263d592b885593d6d6d21aafc";
+var request = require('request');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -34,7 +36,6 @@ router.get('/', function(req, res) {
 });
 
 router.route('/speech')
-
 	.post(function(req, res) {
 		console.log(req.body.text);
 		wit.captureTextIntent(ACCESS_TOKEN, req.body.text, function (err, witRes) {
@@ -49,10 +50,41 @@ router.route('/speech')
 				});
 		    }
 		});
-});
+	});
+
+router.route('/test') 
+	.get(function(req, res) {
+		capOneBalance("55e94a6bf8d8770528e6144e", function(response) {
+			res.json(response);
+		});
+	});
 
 function computeText(witRes, cb) {
+	if (witRes.outcomes) {
+		if (witRes.outcomes[0].intent = "greetings") {
+			cb({ message: "Hello, what can I help you with?" });
+		}
+	} else {
+		cb({ message: "Sorry I didn't quite get that, could you ask something different"});
+	}
+}
 
+function capOneGetAccounts(cb) {
+	request('http://api.reimaginebanking.com/accounts?key=' + CAPONE_KEY, function (error, response, body) {
+	  if (!error && response.statusCode == 200) { 
+	    console.log(JSON.parse(body)); // Show the HTML for the Google homepage. 
+	    cb(JSON.parse(body));
+	  }
+	})
+}
+
+function capOneBalance(account, cb) {
+	request('http://api.reimaginebanking.com/accounts/' + account + '?key=' + CAPONE_KEY, function (error, response, body) {
+	  if (!error && response.statusCode == 200) { 
+	    console.log(JSON.parse(body)); // Show the HTML for the Google homepage. 
+	    cb(JSON.parse(body));
+	  }
+	})
 }
 
 // more routes for our API will happen here
