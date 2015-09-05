d09f57aa8da2032b532e914650789d98235392c6
Jimmy Liu <sicong.liu98@gmail.com>
2015-09-05 13:14:00 -0400

    json

--- server.js
+++ server.js
@@ -7,6 +7,8 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var wit = require('node-wit');
+var ACCESS_TOKEN = "IBENYNTQT2C36HSUMM6Q5EGBPSJ7EUUU";
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -19,10 +21,39 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
+
+router.route('/speech')
+
+	.post(function(req, res) {
+		console.log(req.body.text);
+		wit.captureTextIntent(ACCESS_TOKEN, req.body.text, function (err, witRes) {
+		    if (err) {
+		    	res.json(400, {message: "Error calling Wit.ai"});
+		    	console.log("Error: ", err);
+		    } else { 	
+		    	console.log(JSON.stringify(witRes, null, " "));
+		    	computeText(witRes, function(response) {
+				    console.log(response); 
+				    res.json(response);   
+				});
+		    }
+		});
+});
+
+function computeText(witRes, cb) {
+
+}
 
 // more routes for our API will happen here
 
